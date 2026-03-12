d9896f0b0bf85b45345e812496668272d682e61a
Cyprux <justinprogproj@gmail.com>
2026-03-12 19:02:22 +0800

    Update syntax

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,7 +8,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
-
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddtCommand;
 import seedu.address.logic.commands.ClearCommand;
@@ -20,7 +19,6 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListtCommand;
-
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
